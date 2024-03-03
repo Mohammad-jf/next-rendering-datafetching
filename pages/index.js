08cd6c6211c90c46ebee0f9a818e6cd5412d4f19
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-03-03 23:25:18 +0330

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <>
-      
+      <h3>{data[0]}</h3>
     </>
   );
+}
+
+
+export async function getStaticProps() {
+  const data = ['mohammad hossein'];
+
+
+  return {
+    props: {
+      data
+    }
+  }
 }
