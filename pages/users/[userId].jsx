fa407a2cfd5eaf3be1370c0732dcf9eeaa67f7b9
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-03-11 14:07:23 +0330

    falback true

--- pages/users/[userId].jsx
+++ pages/users/[userId].jsx
@@ -1,9 +1,14 @@
-
+import { useRouter } from 'next/router'
 import React from 'react'
 
 
 
 const UserId = ({ data }) => {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return <h2>FallBack page...</h2>
+    }
     return (
         <div>{data.name}</div>
     )
@@ -12,18 +17,23 @@ const UserId = ({ data }) => {
 export default UserId
 
 
+// getting address for each page and pages length
 export async function getStaticPaths() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
-    
-    let paths = data.map((item) => ({ params: { userId: String(item.id) } }))
+    const userData = data.slice(0, 4)
+
+    let paths = userData.map((item) => ({ params: { userId: String(item.id) } }))
     return {
         paths,
-        fallback: false
+        fallback: true
     }
 }
 
 
+
+
+// get data for each page
 export async function getStaticProps(context) {
     const { params } = context;
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
