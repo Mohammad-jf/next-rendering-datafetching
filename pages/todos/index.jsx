bf189834ee23142f9dedaf3c41a1c9bc493ea401
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-03-23 02:09:19 +0330

    use swr hook

--- pages/todos/index.jsx
+++ pages/todos/index.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
 
 const Todos = () => {
     const [todos, setTodos] = useState(null || []);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(res => res.json())
-            .then(data => setTodos(data))
+    const getData = async (url) => {
+        const res = await fetch(url);
+        const data = res.json();
+        return data
+    }
 
-    }, [])
+    const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/todos', getData);
+
+    useEffect(() => {
+        if (data) {
+            setTodos(data)
+        } else {
+            setTodos([])
+        }
+    }, [data]);
+
+
+    if (error) return <h1>Failed to load</h1>
+    if (isLoading) return <h1>is Loading</h1>
     return (
         <div>
             <ul>
